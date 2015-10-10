304b3ccceee938615fb4960b9967bac50708e827
Christian Liebhardt <christian.liebhardt@arcor.de>
2015-10-10 15:10:53 +0200

    Fix: Hardcoded array bounds

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub fn add_one(data: &mut [f32]) 
+pub fn add_one(data: &mut [f64]) 
 {
-	for i in 0..10
+	for i in 0..data.len()
 	{ 
 		data[i] = data[i] + 1.0;
 	}
@@ -12,4 +12,12 @@ fn add_one_test()
 	let mut result = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
 	add_one(&mut result);
 	assert_eq!(result, [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0]);
+}
+
+#[test]
+fn add_one_test_odd_number_of_elements()
+{
+	let mut result = [1.0, 2.0, 3.0];
+	add_one(&mut result);
+	assert_eq!(result, [2.0, 3.0, 4.0]);
 }
