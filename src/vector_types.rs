c4b25015d110cd5480b4bffb50c9decd7e4edbbb
Christian Liebhardt <christian.liebhardt@arcor.de>
2015-10-10 21:39:42 +0200

    Added logic to partition data into chunks

--- src/vector_types.rs
+++ src/vector_types.rs
@@ -1,4 +1,3 @@
-
 use simd::f32x4;
 
 pub struct Complex
@@ -15,7 +14,53 @@ impl Complex
 	}
 }
 
-#[allow(dead_code)]
+const NUM_CHUNKS: usize = 8;
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+struct Chunk
+{
+	pub start: usize,
+	pub end: usize,
+	pub step_size: usize
+}
+
+impl Chunk
+{
+	fn new(start: usize, end: usize, step_size: usize) -> Chunk
+	{
+		return Chunk { start: start, end: end, step_size: step_size };
+	}
+	
+	fn partition(array_length: usize, step_size: usize) -> Vec<Chunk>
+	{
+		let mut result = Vec::new();
+		if array_length < 1000
+		{
+			result.push(Chunk::new(0, array_length - 1, step_size));
+			return result;
+		}
+		else
+		{
+			let mut chunk_size = array_length / NUM_CHUNKS;
+			chunk_size -= chunk_size % step_size;
+			let mut i = 0;
+			let mut last_start = 0;
+			while i < NUM_CHUNKS
+			{
+				let new_start = 
+					if i != NUM_CHUNKS - 1 { last_start + chunk_size } 
+					else				     { array_length };
+				result.push(Chunk::new(last_start, new_start - 1, step_size));
+				last_start = new_start;
+				i += 1;
+			}
+			
+			return result;
+		}
+	}
+}
+
 pub struct DataVector<'a> 
 {
 	pub data: &'a mut[f32],
@@ -24,7 +69,6 @@ pub struct DataVector<'a>
 	pub is_complex: bool
 }
 
-#[allow(dead_code)]
 impl<'a> DataVector<'a>
 {
 	pub fn new<'b>(data: &'b mut[f32]) -> DataVector<'b>
@@ -140,6 +184,30 @@ impl<'a> DataVector<'a>
 			array[i] = array[i] * real_imag[i % 2];
 		}
 	}
+}
+
+#[test]
+fn partition_small_arrays_test()
+{
+	let result = Chunk::partition(500, 2);
+	assert_eq!(result, vec![Chunk::new(0, 499, 2)]);
+}
+
+#[test]
+fn partition_large_arrays_test()
+{
+	let result = Chunk::partition(6777, 4);
+	// test assumes that the chunk number is set to 8
+	assert_eq!(result, 
+		vec![ Chunk::new(0, 843, 4), 
+		  Chunk::new(844, 1687, 4),
+		  Chunk::new(1688, 2531, 4),
+		  Chunk::new(2532, 3375, 4),
+		  Chunk::new(3376, 4219, 4),
+		  Chunk::new(4220, 5063, 4),
+		  Chunk::new(5064, 5907, 4),
+		  Chunk::new(5908, 6776, 4),
+		 ]);
 }
 
 #[test]
