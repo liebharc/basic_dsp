a14511d2d085a915a4ae9567c8cf1b1fc24c91f8
Christian Liebhardt <christian.liebhardt@keysight.com>
2015-11-15 20:48:19 -0800

    Benchmark results are now again comparable to what we had before since the vector creation isn't benchmarked now anymore but only the operation which we really want to benchmark.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -14,12 +14,82 @@ mod bench {
 		ComplexTimeVector32, 
 		Operation32};
 	use num::complex::Complex32;
+	use std::boxed::Box;
+	
+	pub struct VectorBox<T>
+	{
+		vector: *mut T
+	}
+	
+	impl VectorBox<DataVector32>
+	{
+		fn new() -> VectorBox<DataVector32>
+		{
+			let data = vec![0.0; DEFAULT_DATA_SIZE];
+			let vector = DataVector32::from_interleaved_no_copy(data);
+			VectorBox
+			{
+				vector: Box::into_raw(Box::new(vector))
+			}
+		}
+	}
+	
+	impl VectorBox<RealTimeVector32>
+	{
+		fn new() -> VectorBox<RealTimeVector32>
+		{
+			let data = vec![0.0; DEFAULT_DATA_SIZE];
+			let vector = RealTimeVector32::from_array_no_copy(data);
+			VectorBox
+			{
+				vector: Box::into_raw(Box::new(vector))
+			}
+		}
+	}
+	
+	impl VectorBox<ComplexTimeVector32>
+	{
+		fn new() -> VectorBox<ComplexTimeVector32>
+		{
+			let data = vec![0.0; DEFAULT_DATA_SIZE];
+			let vector = ComplexTimeVector32::from_interleaved_no_copy(data);
+			VectorBox
+			{
+				vector: Box::into_raw(Box::new(vector))
+			}
+		}
+	}
+	
+	#[allow(dead_code)]
+	impl<T> VectorBox<T>
+	{
+		fn execute<F>(&mut self, function: F) -> bool
+			where F: Fn(T) -> T + 'static + Sync
+		{
+			unsafe {
+				let vector = Box::from_raw(self.vector);
+				let result = function(*vector);
+				self.vector = Box::into_raw(Box::new(result));
+			}
+			
+			true
+		}
+	}
+	
+	impl<T> Drop for VectorBox<T>
+	{
+		fn drop(&mut self) {
+			unsafe {
+				let _ = Box::from_raw(self.vector); // make sure that the vector is deleted
+			}
+		}
+	}
 	
 	pub fn add_offset_reference(array: &mut [f32], offset: f32) 
 	{
 		let mut i = 0;
 		while i < array.len()
-		{ 
+		{
 			array[i] = array[i] + offset;
 			i += 1;
 		}
@@ -30,8 +100,8 @@ mod bench {
 	#[bench]
 	fn add_real_one_scalar_32_benchmark(b: &mut Bencher)
 	{
-			b.iter(move|| {
-			let mut data: Box<[f32]> = box [0.0; DEFAULT_DATA_SIZE];
+		let mut data: Box<[f32]> = box [0.0; DEFAULT_DATA_SIZE];
+		b.iter(move|| {
 			add_offset_reference(&mut data, 1.0);
 			return data[0];
 			});
@@ -50,59 +120,52 @@ mod bench {
 	#[bench]
 	fn add_real_vector_32_benchmark(b: &mut Bencher)
 	{
+		let mut vector = VectorBox::<RealTimeVector32>::new();
 		b.iter(|| {
-			let data = vec![0.0; DEFAULT_DATA_SIZE];
-			let result = RealTimeVector32::from_array_no_copy(data);
-			let result = result.real_offset(2.0);
-			return result.delta();;
-			});
+			vector.execute(|v|  { v.real_offset(2.0) } )
+		});
 	}
 	
 	#[bench]
 	fn multi_operations_vector_32_benchmark(b: &mut Bencher)
 	{
+		let mut vector = VectorBox::<DataVector32>::new();
 		b.iter(|| {
-			let data = vec![0.0; DEFAULT_DATA_SIZE];
-			let result = ComplexTimeVector32::from_interleaved_no_copy(data);
-			let result = result.perform_operations(
-				&[Operation32::AddReal(1.0),
-				Operation32::AddComplex(Complex32::new(1.0, 1.0)),
-				Operation32::MultiplyComplex(Complex32::new(-1.0, 1.0))]);
-			return result.delta();
-			});
+			vector.execute(|v|  
+				{
+					  v.perform_operations(
+						&[Operation32::AddReal(1.0),
+						Operation32::AddComplex(Complex32::new(1.0, 1.0)),
+						Operation32::MultiplyComplex(Complex32::new(-1.0, 1.0))])
+				})
+		});
 	}
 	
 	#[bench]
 	fn scale_complex_vector_32_benchmark(b: &mut Bencher)
 	{
+		let mut vector = VectorBox::<ComplexTimeVector32>::new();
 		b.iter(|| {
-			let data = vec![0.0; DEFAULT_DATA_SIZE];
-			let result = ComplexTimeVector32::from_interleaved_no_copy(data);
-			let result = result.complex_scale(Complex32::new(-2.0, 2.0));
-			return result.delta();
-			});
+			vector.execute(|v|  { v.complex_scale(Complex32::new(-2.0, 2.0)) } )
+		});
 	}
 	
 	#[bench]
 	fn abs_real_vector_32_benchmark(b: &mut Bencher)
 	{
+		let mut vector = VectorBox::<RealTimeVector32>::new();
 		b.iter(|| {
-			let data = vec![0.0; DEFAULT_DATA_SIZE];
-			let result = RealTimeVector32::from_array_no_copy(data);
-			let result = result.real_abs();
-			return result.delta();
-			});
+			vector.execute(|v|  { v.real_abs() } )
+		});
 	}
 	
 	#[bench]
 	fn abs_complex_vector_32_benchmark(b: &mut Bencher)
 	{
+		let mut vector = VectorBox::<DataVector32>::new();
 		b.iter(|| {
-			let data = vec![0.0; DEFAULT_DATA_SIZE];
-			let result = ComplexTimeVector32::from_interleaved_no_copy(data);
-			let result = result.complex_abs();
-			return result.delta();
-			});
+			vector.execute(|v|  { v.complex_abs() } )
+		});
 	}
 	
 	/*
