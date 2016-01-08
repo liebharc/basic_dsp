6d56745026824c94d6632d7c7a76860c8c30d9a7
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-01-07 22:26:15 -0800

    Added macro stubs for correlation and interpolation.

--- src/vector_types/correlation_impl.rs
+++ src/vector_types/correlation_impl.rs
@@ -2,6 +2,12 @@ use super::definitions::{
     DataVector,
     VecResult};
 use RealNumber;
+use super::{
+    GenericDataVector,
+    RealTimeVector,
+    RealFreqVector,
+    ComplexTimeVector,
+    ComplexFreqVector};
 
 /// Cross-correlation of data vectors. See also https://en.wikipedia.org/wiki/Cross-correlation
 pub trait CrossCorrelation<T> : DataVector<T> 
@@ -11,3 +17,35 @@ pub trait CrossCorrelation<T> : DataVector<T>
     /// before the operation was performed.
     fn correlate(self, other: &Self) -> VecResult<Self>;
 }
+
+macro_rules! define_correlation_impl {
+    ($($data_type:ident);*) => {
+        $( 
+            impl CrossCorrelation<$data_type> for GenericDataVector<$data_type> {
+                fn correlate(self, _other: &Self) -> VecResult<Self> {
+                    panic!("Panic")
+                }
+            }
+        )*
+    }
+}
+define_correlation_impl!(f32; f64);
+
+macro_rules! define_correlation_forward {
+    ($($name:ident, $data_type:ident);*) => {
+        $( 
+            impl CrossCorrelation<$data_type> for $name<$data_type> {
+                fn correlate(self, other: &Self) -> VecResult<Self> {
+                    Self::from_genres(self.to_gen().correlate(other.to_gen_borrow()))
+                }
+            }
+        )*
+    }
+}
+
+define_correlation_forward!(
+    RealTimeVector, f32; RealTimeVector, f64;
+    ComplexTimeVector, f32; ComplexTimeVector, f64;
+    RealFreqVector, f32; RealFreqVector, f64;
+    ComplexFreqVector, f32; ComplexFreqVector, f64
+);
--- src/vector_types/interpolation_impl.rs
+++ src/vector_types/interpolation_impl.rs
@@ -3,10 +3,51 @@ use super::definitions::{
     VecResult};
 use RealNumber;
 use conv_types::RealTimeConvFunction;
+use super::{
+    GenericDataVector,
+    RealTimeVector,
+    RealFreqVector,
+    ComplexTimeVector,
+    ComplexFreqVector};
 
 /// Provides a interpolation operation for data vectors.
+/// # Unstable
+/// This functionality has been recently added in order to find out if the definitions are consistent.
+/// However the actual implementation is lacking tests.
 pub trait Interpolation<T> : DataVector<T> 
     where T : RealNumber {
     /// Interpolates `self` with the convolution function `function`.
-    fn interpolate(self, function: RealTimeConvFunction<T>, interpolation_factor: T, delay: T) -> VecResult<Self>;
+    fn interpolate(self, function: &RealTimeConvFunction<T>, interpolation_factor: T, delay: T) -> VecResult<Self>;
 }
+
+macro_rules! define_interpolation_impl {
+    ($($data_type:ident);*) => {
+        $( 
+            impl Interpolation<$data_type> for GenericDataVector<$data_type> {
+                fn interpolate(self, _function: &RealTimeConvFunction<$data_type>, _interpolation_factor: $data_type, _delay: $data_type) -> VecResult<Self> {
+                    panic!("Panic")
+                }
+            }
+        )*
+    }
+}
+define_interpolation_impl!(f32; f64);
+
+macro_rules! define_interpolation_forward {
+    ($($name:ident, $data_type:ident);*) => {
+        $( 
+            impl Interpolation<$data_type> for $name<$data_type> {
+                fn interpolate(self, function: &RealTimeConvFunction<$data_type>, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
+                    Self::from_genres(self.to_gen().interpolate(function, interpolation_factor, delay))
+                }
+            }
+        )*
+    }
+}
+
+define_interpolation_forward!(
+    RealTimeVector, f32; RealTimeVector, f64;
+    ComplexTimeVector, f32; ComplexTimeVector, f64;
+    RealFreqVector, f32; RealFreqVector, f64;
+    ComplexFreqVector, f32; ComplexFreqVector, f64
+);
