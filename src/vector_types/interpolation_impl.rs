74b3a5ace2719b9d4637eb3498bd93777a6669b2
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-01-07 22:47:53 -0800

    Filled interpolation method with some code. Basically just to outline how the actual implementation could look like. Untested and therefore won't work.

--- src/vector_types/interpolation_impl.rs
+++ src/vector_types/interpolation_impl.rs
@@ -17,15 +17,36 @@ use super::{
 pub trait Interpolation<T> : DataVector<T> 
     where T : RealNumber {
     /// Interpolates `self` with the convolution function `function`.
-    fn interpolate(self, function: &RealTimeConvFunction<T>, interpolation_factor: T, delay: T) -> VecResult<Self>;
+    fn interpolate(self, function: &RealTimeConvFunction<T>, interpolation_factor: T, delay: T, len: usize) -> VecResult<Self>;
 }
 
 macro_rules! define_interpolation_impl {
     ($($data_type:ident);*) => {
         $( 
             impl Interpolation<$data_type> for GenericDataVector<$data_type> {
-                fn interpolate(self, _function: &RealTimeConvFunction<$data_type>, _interpolation_factor: $data_type, _delay: $data_type) -> VecResult<Self> {
-                    panic!("Panic")
+                fn interpolate(mut self, function: &RealTimeConvFunction<$data_type>, interpolation_factor: $data_type, delay: $data_type, conv_len: usize) -> VecResult<Self> {
+                    {
+                        let len = self.len();
+                        let new_len = (len as $data_type * interpolation_factor) as usize;
+                        let data = &self.data[0..len];
+                        let temp = temp_mut!(self, new_len);
+                        let mut i = 0;
+                        for num in temp {
+                            let k = (i as $data_type / interpolation_factor).round() as usize;
+                            let start = if k > conv_len { i - conv_len } else { 0 };
+                            let end = if k + conv_len < len { i + conv_len } else { len };
+                            let mut sum = 0.0;
+                            let center = k as $data_type;
+                            let mut j = start as $data_type - center - delay;
+                            for c in &data[start..end] {
+                                sum = sum + (*c) * function.calc(j * interpolation_factor);
+                                j += 1.0;
+                            }
+                            (*num) = sum;
+                            i += 1;
+                        }
+                    }
+                    Ok(self.swap_data_temp())
                 }
             }
         )*
@@ -37,8 +58,8 @@ macro_rules! define_interpolation_forward {
     ($($name:ident, $data_type:ident);*) => {
         $( 
             impl Interpolation<$data_type> for $name<$data_type> {
-                fn interpolate(self, function: &RealTimeConvFunction<$data_type>, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
-                    Self::from_genres(self.to_gen().interpolate(function, interpolation_factor, delay))
+                fn interpolate(self, function: &RealTimeConvFunction<$data_type>, interpolation_factor: $data_type, delay: $data_type, len: usize) -> VecResult<Self> {
+                    Self::from_genres(self.to_gen().interpolate(function, interpolation_factor, delay, len))
                 }
             }
         )*
