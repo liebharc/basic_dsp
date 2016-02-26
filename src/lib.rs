52ebf5625a56d6f8ec433cb5effdcd62b460fdb7
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-02-25 22:42:30 -0800

    Updated crate description.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 #![feature(cfg_target_feature)]
 //! Basic digital signal processing (DSP) operations
 //!
-//! The basic building blocks are 1xN (one times N) or Nx1 real or compelex vectors where N is typically at least in the order
+//! The basic building blocks are 1xN (one times N) or Nx1 real or complex vectors where N is typically at least in the order
 //! of magnitude of a couple of thousand elements. This crate tries to balance between a clear API and performance in terms of processing speed.
 //! This project started as small pet project to learn more about DSP, CPU architecture and Rust. Since learning
 //! involves making mistakes, don't expect things to be flawless or even close to flawless.
