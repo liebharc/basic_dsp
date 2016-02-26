99ff20e59b710f330ae35c91f1198a2aa342fce1
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-02-25 22:28:46 -0800

    Added an operation for hermite interpolation. Definition seems to be different to Octave for the corner cases (beginning of a vector, end of a vector). This operation also isn't monotone https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Monotone_cubic_interpolation

--- src/vector_types/interpolation_impl.rs
+++ src/vector_types/interpolation_impl.rs
@@ -61,13 +61,20 @@ pub trait Interpolation<T> : DataVector<T>
 }
 
 /// Provides interpolation operations which are only applicable for real data vectors.
+/// # Failures
+/// All operations in this trait fail with `VectorMustBeReal` if the vector isn't in the real number space.
 pub trait RealInterpolation<T> : DataVector<T>
     where T: RealNumber {
     
     // Piecewise cubic hermite interpolation between samples.
-    fn interpolate_pchip(self, interpolation_factor: T, delay: T) -> VecResult<Self>;
+    /// # Unstable
+    /// Algorithm might need to be revised.
+    /// This operation an `interpolate_lin` might be merged into one function with an additional argument in future.
+    fn interpolate_hermite(self, interpolation_factor: T, delay: T) -> VecResult<Self>;
     
     /// Linear interpolation between samples.
+    /// # Unstable
+    /// This operation an `interpolate_hermite` might be merged into one function with an additional argument in future.
     fn interpolate_lin(self, interpolation_factor: T, delay: T) -> VecResult<Self>;
 }
 
@@ -396,12 +403,111 @@ macro_rules! define_interpolation_impl {
             }
             
             impl RealInterpolation<$data_type> for GenericDataVector<$data_type> {
-                fn interpolate_lin(self, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
-                    panic!("Panic")
+                fn interpolate_lin(mut self, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
+                    {
+                        assert_real!(self);
+                        let data_len = self.len();
+                        let dest_len = ((data_len - 1) as $data_type * interpolation_factor).round() as usize + 1;
+                        let data = &self.data[0..data_len];
+                        let dest = temp_mut!(self, dest_len);
+                        let mut i = 0.0;
+    
+                        for num in &mut dest[0..dest_len-1] {
+                            let rounded = i / interpolation_factor + delay;
+                            let beforef = rounded.floor();
+                            let before = beforef as usize;
+                            let next = before + 1;
+                            let x0 = beforef;
+                            let y0 = data[before];
+                            let y1 = data[next];
+                            let x = rounded;
+                            (*num) =  y0 + (y1 - y0) * (x - x0);
+                            i += 1.0; 
+                        }
+                        dest[dest_len-1] = data[data_len - 1];
+                        self.valid_len = dest_len;
+                    }
+                    Ok(self.swap_data_temp())
                 }
                 
-                fn interpolate_pchip(self, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
-                    panic!("Panic")
+                fn interpolate_hermite(mut self, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
+                    {
+                        assert_real!(self);
+                        // Literature: http://paulbourke.net/miscellaneous/interpolation/
+                        let data_len = self.len();
+                        let dest_len = ((data_len - 1) as $data_type * interpolation_factor).round() as usize + 1;
+                        let data = &self.data[0..data_len];
+                        let dest = temp_mut!(self, dest_len);
+                        let mut i = 0.0;
+                        let start = ((1.0 - delay) * interpolation_factor).ceil() as usize;
+                        let end = start + 1;
+                        for num in &mut dest[0..start] {
+                            let rounded = i / interpolation_factor + delay;
+                            let beforef = rounded.floor();
+                            let before = beforef as usize;
+                            assert_eq!(before, 0);
+                            let next = before + 1;
+                            let next_next = next + 1;
+                            let y1 = data[before];
+                            let y2 = data[next];
+                            let y3 = data[next_next];
+                            let x =  rounded - beforef;
+                            let y0 = y1 - (y2 - y1); 
+                            let x2 = x * x;
+                            let a0 = -0.5 * y0 + 1.5 * y1 - 1.5 * y2 + 0.5 * y3;
+                            let a1 = y0 - 2.5 * y1 + 2.0 * y2 - 0.5 * y3;
+                            let a2 = -0.5 * y0 + 0.5 * y2;
+                            let a3 = y1;
+                            
+                            (*num) =  (a0 * x * x2) + (a1 * x2) + (a2 * x) + a3;
+                            i += 1.0; 
+                        }
+                        
+                        for num in &mut dest[start..dest_len-end] {
+                            let rounded = i / interpolation_factor + delay;
+                            let beforef = rounded.floor();
+                            let before = beforef as usize;
+                            let before_before = before - 1;
+                            let next = before + 1;
+                            let next_next = next + 1;
+                            let y0 = data[before_before];
+                            let y1 = data[before];
+                            let y2 = data[next];
+                            let y3 = data[next_next];
+                            let x =  rounded - beforef;
+                            let x2 = x * x;
+                            let a0 = -0.5 * y0 + 1.5 * y1 - 1.5 * y2 + 0.5 * y3;
+                            let a1 = y0 - 2.5 * y1 + 2.0 * y2 - 0.5 * y3;
+                            let a2 = -0.5 * y0 + 0.5 * y2;
+                            let a3 = y1;
+                            
+                            (*num) =  (a0 * x * x2) + (a1 * x2) + (a2 * x) + a3;
+                            i += 1.0; 
+                        }
+                        
+                        for num in &mut dest[dest_len-end..dest_len] {
+                            let rounded = i / interpolation_factor + delay;
+                            let beforef = rounded.floor();
+                            let before = beforef as usize;
+                            assert!(before + 2 >= data_len);
+                            let before_before = before - 1;
+                            let y0 = data[before_before];
+                            let y1 = data[before];
+                            let y2 = if before < data_len - 1 { data[before + 1] } else { y1 + (y1 - y0) };
+                            let y3 = if before < data_len - 2 { data[before + 2] } else { y2 + (y2 - y1) };
+                            let x =  rounded - beforef;
+                            let x2 = x * x;
+                            let a0 = -0.5 * y0 + 1.5 * y1 - 1.5 * y2 + 0.5 * y3;
+                            let a1 = y0 - 2.5 * y1 + 2.0 * y2 - 0.5 * y3;
+                            let a2 = -0.5 * y0 + 0.5 * y2;
+                            let a3 = y1;
+                            
+                            (*num) =  (a0 * x * x2) + (a1 * x2) + (a2 * x) + a3;
+                            i += 1.0; 
+                        }
+                        self.valid_len = dest_len;
+                    }
+                    Ok(self.swap_data_temp())
                 }
             }
         )*
@@ -437,8 +543,8 @@ macro_rules! define_real_only_interpolation_forward {
                     Self::from_genres(self.to_gen().interpolate_lin(interpolation_factor, delay))
                 }
                 
-                fn interpolate_pchip(self, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
-                    Self::from_genres(self.to_gen().interpolate_pchip(interpolation_factor, delay))
+                fn interpolate_hermite(self, interpolation_factor: $data_type, delay: $data_type) -> VecResult<Self> {
+                    Self::from_genres(self.to_gen().interpolate_hermite(interpolation_factor, delay))
                 }
             }
         )*
@@ -541,15 +647,26 @@ mod tests {
     
     #[test]
     fn hermit_spline_test() {
-        // http://stackoverflow.com/questions/8557098/cubic-hermite-spline-behaving-strangely
-        // https://en.wikipedia.org/wiki/Cubic_Hermite_spline
         let time = RealFreqVector32::from_array(&[-1.0, -2.0, -1.0, 0.0, 1.0, 3.0, 4.0]);
-        let result = time.interpolate_pchip(4.0, 0.0).unwrap();
+        let result = time.interpolate_hermite(4.0, 0.0).unwrap();
         let expected = [
             -1.0000, -1.4375, -1.7500, -1.9375, -2.0000, -1.8906, -1.6250, -1.2969,
             -1.0000, -0.7500, -0.5000, -0.2500, 0.0, 0.2344, 0.4583, 0.7031,
             1.0000, 1.4375, 2.0000, 2.5625, 3.0000, 3.3203, 3.6042, 3.8359, 4.0];
-        assert_eq_tol(result.data(), &expected, 0.1);
+        assert_eq_tol(
+            &result.data()[4..expected.len()-4], 
+            &expected[4..expected.len()-4], 
+            6e-2);
+    }
+    
+    #[test]
+    fn hermit_spline_test_linear_increment() {
+        let time = RealFreqVector32::from_array(&[-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0]);
+        let result = time.interpolate_hermite(3.0, 0.0).unwrap();
+        let expected = [
+            -3.0, -2.666, -2.333, -2.0, -1.666, -1.333, -1.0, -0.666, -0.333, 0.0,
+            0.333, 0.666, 1.0, 1.333, 1.666, 2.0, 2.333, 2.666, 3.0];
+        assert_eq_tol(result.data(), &expected, 5e-3);
     }
     
     #[test]
