27b73d88eece15176f9728409c35659f510544ae
Christian Liebhardt <christian.liebhardt@arcor.de>
2016-06-04 12:44:15 -0700

    Removed target-cpu=native from rustc wrapper scripts so that on a CPU with AVX instructions it is still possible to build a version of the lib which doesn't use AVX instructions

--- rustc.bat
+++ rustc.bat
@@ -1,4 +1,4 @@
 @echo off
 rem f32x8/avx causes a crash right now. See comment on https://github.com/huonw/simd/pull/18
-rem rustc.exe -C target-cpu=native -C target-feature=+sse2,+sse3,+avx2,+avx %*
-rustc.exe -C target-cpu=native -C target-feature=+sse2,+sse3 %*
+rem rustc.exe -C target-feature=+sse2,+sse3,+avx2,+avx %*
+rustc.exe -C target-feature=+sse2,+sse3 %*
