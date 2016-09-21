a6d32555d2669f6ac495b31439a3ce7cf2162902
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-09-20 20:48:59 -0700

    Added real ops for vec2

--- vector/src/vector_types2/real/mod.rs
+++ vector/src/vector_types2/real/mod.rs
@@ -1,2 +1,4 @@
 mod real_to_complex;
 pub use self::real_to_complex::*;
+mod real;
+pub use self::real::*;
--- vector/src/vector_types2/real/real.rs
+++ vector/src/vector_types2/real/real.rs
@@ -1,3 +1,11 @@
+use RealNumber;
+use multicore_support::*;
+use simd_extensions::*;
+use super::super::{
+    Vector,
+    DspVec, ToSliceMut,
+    Domain, RealNumberSpace,
+};
 
 /// Operations on real types.
 ///
@@ -7,17 +15,18 @@
 /// To avoid this it's recommended to use the `to_real_time_vec`, `to_real_freq_vec`
 /// `to_complex_time_vec` and `to_complex_freq_vec` constructor methods since
 /// the resulting types will already check at compile time (using the type system) that the data is real.
-pub trait RealOps {
+pub trait RealOps<T>
+    where T: RealNumber {
 	/// Gets the absolute value of all vector elements.
     /// # Example
     ///
     /// ```
-    /// use basic_dsp_vector::{RealTimeVector32, RealVectorOps, DataVec, RealIndex};
-    /// let vector = RealTimeVector32::from_array(&[1.0, -2.0]);
-    /// let result = vector.abs().expect("Ignoring error handling in examples");
-    /// assert_eq!([1.0, 2.0], result.real(0..));
+    /// use basic_dsp_vector::vector_types2::*;
+    /// let mut vector = vec!(1.0, -2.0).to_real_time_vec();
+    /// vector.abs();
+    /// assert_eq!([1.0, 2.0], vector[..]);
     /// ```
-    fn abs(self) -> TransRes<Self>;
+    fn abs(&mut self);
 
 	/// Each value in the vector is dividable by the divisor and the remainder is stored in the resulting
     /// vector. This the same a modulo operation or to phase wrapping.
@@ -25,12 +34,12 @@ pub trait RealOps {
     /// # Example
     ///
     /// ```
-    /// use basic_dsp_vector::{RealTimeVector32, RealVectorOps, DataVec, RealIndex};
-    /// let vector = RealTimeVector32::from_array(&[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
-    /// let result = vector.wrap(4.0).expect("Ignoring error handling in examples");
-    /// assert_eq!([1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0], result.real(0..));
+    /// use basic_dsp_vector::vector_types2::*;
+    /// let mut vector = vec!(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0).to_real_time_vec();
+    /// vector.wrap(4.0);
+    /// assert_eq!([1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0], vector[..]);
     /// ```
-    fn wrap(self, divisor: T) -> TransRes<Self>;
+    fn wrap(&mut self, divisor: T);
 
     /// This function corrects the jumps in the given vector which occur due to wrap or modulo operations.
     /// This will undo a wrap operation only if the deltas are smaller than half the divisor.
@@ -38,10 +47,60 @@ pub trait RealOps {
     /// # Example
     ///
     /// ```
-    /// use basic_dsp_vector::{RealTimeVector32, RealVectorOps, DataVec, RealIndex};
-    /// let vector = RealTimeVector32::from_array(&[1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0]);
-    /// let result = vector.unwrap(4.0).expect("Ignoring error handling in examples");
-    /// assert_eq!([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0], result.real(0..));
+    /// use basic_dsp_vector::vector_types2::*;
+    /// let mut vector = vec!(1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.).to_real_time_vec();
+    /// vector.unwrap(4.0);
+    /// assert_eq!([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0], vector[..]);
     /// ```
-    fn unwrap(self, divisor: T) -> TransRes<Self>;
+    fn unwrap(&mut self, divisor: T);
+}
+
+macro_rules! assert_real {
+    ($self_: ident) => {
+        if $self_.is_complex() {
+            $self_.valid_len = 0;
+            return;
+        }
+    }
+}
+
+impl<S, T, N, D> RealOps<T> for DspVec<S, T, N, D>
+    where S: ToSliceMut<T>,
+          T: RealNumber,
+          N: RealNumberSpace,
+          D: Domain {
+      fn abs(&mut self) {
+          assert_real!(self);
+          self.simd_real_operation(|x, _arg| (x * x).sqrt(), |x, _arg| x.abs(), (), Complexity::Small);
+      }
+
+      fn wrap(&mut self, divisor: T) {
+          assert_real!(self);
+          self.pure_real_operation(|x, y| x % y, divisor, Complexity::Small);
+      }
+
+      fn unwrap(&mut self, divisor: T) {
+          assert_real!(self);
+          let data_length = self.len();
+          let mut data = self.data.to_slice_mut();
+          let mut i = 0;
+          let mut j = 1;
+          let half = divisor / T::from(2.0).unwrap();
+          while j < data_length {
+              let mut diff = data[j] - data[i];
+              if diff > half {
+                  diff = diff % divisor;
+                  diff = diff - divisor;
+                  data[j] = data[i] + diff;
+              }
+              else if diff < -half {
+                  diff = diff % divisor;
+                  diff = diff + divisor;
+                  data[j] = data[i] + diff;
+              }
+
+              i += 1;
+              j += 1;
+          }
+      }
 }
