c79668cbe4c03f2bcec661190704a4bbd92875c5
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-10-07 21:47:15 -0700

    More interpolations

--- vector/src/vector_types2/time_freq/interpolation.rs
+++ vector/src/vector_types2/time_freq/interpolation.rs
@@ -9,7 +9,8 @@ use simd_extensions::*;
 use super::super::{
 	array_to_complex, array_to_complex_mut,
 	VoidResult, DspVec, Domain, NumberSpace, ToSliceMut,
-	GenDspVec, ToDspVector, DataDomain, Buffer, Vector
+	GenDspVec, ToDspVector, DataDomain, Buffer, Vector,
+    RealNumberSpace
 };
 use std::mem;
 
@@ -49,8 +50,7 @@ pub trait InterpolationOps<S, T>
 		where B: Buffer<S, T>;
 
     /// Decimates or downsamples `self`. `decimatei` is the inverse function to `interpolatei`.
-    fn decimatei<B>(&mut self, buffer: &mut B, decimation_factor: u32, delay: u32)
-		where B: Buffer<S, T>;
+    fn decimatei(&mut self, decimation_factor: u32, delay: u32);
 }
 
 /// Provides interpolation operations which are only applicable for real data vectors.
@@ -161,7 +161,6 @@ impl<S, T, N, D> DspVec<S, T, N, D>
 				  RMul: Fn(T::Reg, T::Reg) -> T::Reg,
 				  RSum: Fn(T::Reg) -> TT,
 				  B: Buffer<S, T> {
-		  let len = self.len();
 		  let mut temp = buffer.get(new_len);
 		  {
 			  let step = if self.is_complex() { 2 } else { 1 };
@@ -276,8 +275,7 @@ impl<S, T, N, D> InterpolationOps<S, T> for DspVec<S, T, N, D>
 		  D: Domain {
   fn interpolatef<B>(&mut self, buffer: &mut B, function: &RealImpulseResponse<T>, interpolation_factor: T, delay: T, conv_len: usize)
   	where B: Buffer<S, T> {
-
-	let delay = delay / self.delta;
+    let delay = delay / self.delta;
   	let len = self.len();
   	let points_half = self.points() / 2;
   	let conv_len =
@@ -354,18 +352,201 @@ impl<S, T, N, D> InterpolationOps<S, T> for DspVec<S, T, N, D>
 
   fn interpolatei<B>(&mut self, buffer: &mut B, function: &RealFrequencyResponse<T>, interpolation_factor: u32) -> VoidResult
   	where B: Buffer<S, T> {
-	  panic!("Panic")
+    panic!("Panic")
+    /*if interpolation_factor <= 1 {
+        return Ok(());
+    }
+
+    if !function.is_symmetric() &&!self.is_complex() {
+        Err(ErrorReason::ArgumentFunctionMustBeSymmetric);
+    }
+
+    let is_complex = self.is_complex();
+    self.zero_interleave_b(buffer, interpolation_factor);
+    let mut freq = self.fft(buffer);
+    let points = freq.points();
+    let interpolation_factorf = T::from(interpolation_factor).unwrap();
+    /*freq.multiply_function_priv(
+        function.is_symmetric(),
+        interpolation_factorf,
+        |array|array_to_complex_mut(array),
+        function,
+        |f,x|Complex::<T>::new(f.calc(x), T::zero()));*/
+    freq.ifft_shift(buffer);
+    let mut time = freq.plain_ifft(buffer);
+    time.scale(Complex::<T>::new(T::from(1.0 / points).unwrap(), T::zero()));
+    if is_complex {
+        time
+    } else {
+        time.to_real()
+    }*/
   }
 
-  fn decimatei<B>(&mut self, buffer: &mut B, decimation_factor: u32, delay: u32)
-  	where B: Buffer<S, T> {
-	  panic!("Panic")
+  fn decimatei(&mut self, decimation_factor: u32, delay: u32) {
+        let mut i = delay as usize;
+        let mut j = 0;
+        let len = self.len();
+        let points = self.points();
+        let is_complex = self.is_complex();
+        let data = self.data.to_slice_mut();
+        if is_complex {
+            let mut data = array_to_complex_mut(&mut data[0..len]);
+            let decimation_factor = decimation_factor as usize;
+            while i < points {
+                data[j] = data[i];
+                i += decimation_factor;
+                j += 1;
+            }
+            self.valid_len = j * 2;
+        }
+        else {
+            let mut data = &mut data[0..len];
+            let decimation_factor = decimation_factor as usize;
+            while i < len {
+                data[j] = data[i];
+                i += decimation_factor;
+                j += 1;
+            }
+            self.valid_len = j;
+        }
+  }
+}
+
+impl<S, T, N, D> RealInterpolationOps<S, T> for DspVec<S, T, N, D>
+	where S: ToSliceMut<T>,
+		  T: RealNumber,
+		  N: RealNumberSpace,
+		  D: Domain {
+    fn interpolate_lin<B>(&mut self, buffer: &mut B, interpolation_factor: T, delay: T)
+        where B: Buffer<S, T> {
+      let data_len = self.len();
+      let dest_len = (T::from(data_len - 1).unwrap() * interpolation_factor).round().to_usize().unwrap() + 1;
+      let mut temp = buffer.get(dest_len);
+      {
+          if self.is_complex() {
+              self.valid_len = 0;
+              return;
+          }
+          let data = self.data.to_slice();
+          let mut temp = temp.to_slice_mut();
+          let data = &data[0..data_len];
+          let dest = &mut temp[0..dest_len];
+          let mut i = T::zero();
+
+          for num in &mut dest[0..dest_len-1] {
+              let rounded = i / interpolation_factor + delay;
+              let beforef = rounded.floor();
+              let before = beforef.to_usize().unwrap();
+              let next = before + 1;
+              let x0 = beforef;
+              let y0 = data[before];
+              let y1 = data[next];
+              let x = rounded;
+              (*num) =  y0 + (y1 - y0) * (x - x0);
+              i = i + T::one();
+          }
+          dest[dest_len-1] = data[data_len - 1];
+          self.valid_len = dest_len;
+      }
+      mem::swap(&mut self.data, &mut temp);
+      buffer.free(temp);
+  }
+
+  fn interpolate_hermite<B>(&mut self, buffer: &mut B, interpolation_factor: T, delay: T)
+        where B: Buffer<S, T> {
+      let data_len = self.len();
+      let dest_len = (T::from(data_len - 1).unwrap() * interpolation_factor).round().to_usize().unwrap() + 1;
+      let mut temp = buffer.get(dest_len);
+      {
+          if self.is_complex() {
+              self.valid_len = 0;
+              return;
+          }
+          let data = self.data.to_slice();
+          let mut temp = temp.to_slice_mut();
+          // Literature: http://paulbourke.net/miscellaneous/interpolation/
+          let data = &data[0..data_len];
+          let dest = &mut temp[0..dest_len];
+          let mut i = T::zero();
+          let start = ((T::one() - delay) * interpolation_factor).ceil().to_usize().unwrap();
+          let end = start + 1;
+          let half = T::from(0.5).unwrap();
+          let one_point_five = T::from(1.5).unwrap();
+          let two = T::from(2.0).unwrap();
+          let two_point_five = T::from(2.5).unwrap();
+          for num in &mut dest[0..start] {
+              let rounded = i / interpolation_factor + delay;
+              let beforef = rounded.floor();
+              let before = beforef.to_usize().unwrap();
+              assert_eq!(before, 0);
+              let next = before + 1;
+              let next_next = next + 1;
+              let y1 = data[before];
+              let y2 = data[next];
+              let y3 = data[next_next];
+              let x =  rounded - beforef;
+              let y0 = y1 - (y2 - y1);
+              let x2 = x * x;
+              let a0 = -half * y0 + one_point_five * y1 - one_point_five * y2 + half * y3;
+              let a1 = y0 - two_point_five * y1 + two * y2 - half * y3;
+              let a2 = -half * y0 + half * y2;
+              let a3 = y1;
+
+              (*num) =  (a0 * x * x2) + (a1 * x2) + (a2 * x) + a3;
+              i = i + T::one();
+          }
+
+          for num in &mut dest[start..dest_len-end] {
+              let rounded = i / interpolation_factor + delay;
+              let beforef = rounded.floor();
+              let before = beforef.to_usize().unwrap();
+              let before_before = before - 1;
+              let next = before + 1;
+              let next_next = next + 1;
+              let y0 = data[before_before];
+              let y1 = data[before];
+              let y2 = data[next];
+              let y3 = data[next_next];
+              let x =  rounded - beforef;
+              let x2 = x * x;
+              let a0 = -half * y0 + one_point_five * y1 - one_point_five * y2 + half * y3;
+              let a1 = y0 - two_point_five * y1 + two * y2 - half * y3;
+              let a2 = -half * y0 + half * y2;
+              let a3 = y1;
+
+              (*num) =  (a0 * x * x2) + (a1 * x2) + (a2 * x) + a3;
+              i = i + T::one();
+          }
+
+          for num in &mut dest[dest_len-end..dest_len] {
+              let rounded = i / interpolation_factor + delay;
+              let beforef = rounded.floor();
+              let before = beforef.to_usize().unwrap();
+              assert!(before + 2 >= data_len);
+              let before_before = before - 1;
+              let y0 = data[before_before];
+              let y1 = data[before];
+              let y2 = if before < data_len - 1 { data[before + 1] } else { y1 + (y1 - y0) };
+              let y3 = if before < data_len - 2 { data[before + 2] } else { y2 + (y2 - y1) };
+              let x =  rounded - beforef;
+              let x2 = x * x;
+              let a0 = -half * y0 + one_point_five * y1 - one_point_five * y2 + half * y3;
+              let a1 = y0 - two_point_five * y1 + two * y2 - half * y3;
+              let a2 = -half * y0 + half * y2;
+              let a3 = y1;
+
+              (*num) =  (a0 * x * x2) + (a1 * x2) + (a2 * x) + a3;
+              i = i + T::one();
+          }
+      }
+      self.valid_len = dest_len;
+      mem::swap(&mut self.data, &mut temp);
+      buffer.free(temp);
   }
 }
 
 #[cfg(test)]
 mod tests {
-    use num::complex::Complex32;
     use conv_types::*;
 	use super::super::super::*;
     use RealNumber;
@@ -437,47 +618,50 @@ mod tests {
              0.00000, 0.62201, 1.00000, 0.62201, 0.00000, 0.16667];
         assert_eq_tol(&result[..], &expected, 0.1);
     }
-/*
+
     #[test]
     fn decimatei_test() {
-        let time = ComplexTimeVector32::from_interleaved(&[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0]);
-        let result = time.decimatei(2, 1).unwrap();
+        let mut time = vec!(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0).to_complex_time_vec();
+        time.decimatei(2, 1);
         let expected = [2.0, 3.0, 6.0, 7.0, 10.0, 11.0];
-        assert_eq_tol(result.interleaved(0..), &expected, 0.1);
+        assert_eq_tol(&time[..], &expected, 0.1);
     }
 
     #[test]
     fn hermit_spline_test() {
-        let time = RealFreqVector32::from_array(&[-1.0, -2.0, -1.0, 0.0, 1.0, 3.0, 4.0]);
-        let result = time.interpolate_hermite(4.0, 0.0).unwrap();
+        let mut time = vec!(-1.0, -2.0, -1.0, 0.0, 1.0, 3.0, 4.0).to_real_freq_vec();
+        let mut buffer = SingleBuffer::new();
+        time.interpolate_hermite(&mut buffer, 4.0, 0.0);
         let expected = [
             -1.0000, -1.4375, -1.7500, -1.9375, -2.0000, -1.8906, -1.6250, -1.2969,
             -1.0000, -0.7500, -0.5000, -0.2500, 0.0, 0.2344, 0.4583, 0.7031,
             1.0000, 1.4375, 2.0000, 2.5625, 3.0000, 3.3203, 3.6042, 3.8359, 4.0];
         assert_eq_tol(
-            &result.real(0..)[4..expected.len()-4],
+            &time[4..expected.len()-4],
             &expected[4..expected.len()-4],
             6e-2);
     }
 
     #[test]
     fn hermit_spline_test_linear_increment() {
-        let time = RealFreqVector32::from_array(&[-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0]);
-        let result = time.interpolate_hermite(3.0, 0.0).unwrap();
+        let mut time = vec!(-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0).to_real_freq_vec();
+        let mut buffer = SingleBuffer::new();
+        time.interpolate_hermite(&mut buffer, 3.0, 0.0);
         let expected = [
             -3.0, -2.666, -2.333, -2.0, -1.666, -1.333, -1.0, -0.666, -0.333, 0.0,
             0.333, 0.666, 1.0, 1.333, 1.666, 2.0, 2.333, 2.666, 3.0];
-        assert_eq_tol(result.real(0..), &expected, 5e-3);
+        assert_eq_tol(&time[..], &expected, 5e-3);
     }
 
     #[test]
     fn linear_test() {
-        let time = RealFreqVector32::from_array(&[-1.0, -2.0, -1.0, 0.0, 1.0, 3.0, 4.0]);
-        let result = time.interpolate_lin(4.0, 0.0).unwrap();
+        let mut time = vec!(-1.0, -2.0, -1.0, 0.0, 1.0, 3.0, 4.0).to_real_freq_vec();
+        let mut buffer = SingleBuffer::new();
+        time.interpolate_lin(&mut buffer, 4.0, 0.0);
         let expected = [
             -1.0000, -1.2500, -1.5000, -1.7500, -2.0000, -1.7500, -1.5000, -1.2500,
             -1.0000, -0.7500, -0.5000, -0.2500, 0.0, 0.2500, 0.5000, 0.7500,
              1.0000, 1.5000, 2.0000, 2.5000, 3.0000, 3.2500, 3.5000, 3.7500, 4.0];
-        assert_eq_tol(result.real(0..), &expected, 0.1);
-    }*/
+        assert_eq_tol(&time[..], &expected, 0.1);
+    }
 }
