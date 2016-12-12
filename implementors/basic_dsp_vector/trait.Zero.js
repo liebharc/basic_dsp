5b3ddef55a505d1392f337268d6b5c735071c304
Christian Liebhardt <liebharc@users.noreply.github.com>
2016-12-11 16:52:50 -0800

    Doc update

--- implementors/basic_dsp_vector/trait.Zero.js
+++ implementors/basic_dsp_vector/trait.Zero.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["num"] = [];
+implementors["basic_dsp_vector"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
