d125b49c390d136a6673c2b84b5149fea0b0e58a
Christian Liebhardt <christian.liebhardt@arcor.de>
2019-02-20 17:47:27 +0100

    Doc update

--- aliases.js
+++ aliases.js
@@ -1,4 +1,4 @@
 var ALIASES = {};
-ALIASES['basic_dsp'] = {};
-ALIASES['basic_dsp_matrix'] = {};
-ALIASES['basic_dsp_vector'] = {};
+ALIASES["basic_dsp"] = {};
+ALIASES["basic_dsp_matrix"] = {};
+ALIASES["basic_dsp_vector"] = {};
