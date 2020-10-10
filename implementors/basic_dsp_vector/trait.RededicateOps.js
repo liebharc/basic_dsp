365c757a17ea05f4214b7ec490311e4d8122cb8f
Christian Liebhardt <christian.liebhardt@arcor.de>
2020-10-10 12:39:34 +0200

    Doc update

--- implementors/basic_dsp_vector/trait.RededicateOps.js
+++ implementors/basic_dsp_vector/trait.RededicateOps.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["basic_dsp_vector"] = [];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
