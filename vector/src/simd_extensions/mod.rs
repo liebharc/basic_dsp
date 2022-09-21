7ac923662e6e839036376d9aaf0a8d4083ff7ffa
Christian Liebhardt <christian.liebhardt@arcor.de>
2022-09-21 18:21:39 +0200

    We're avoiding to call is_x86_feature_detected if avx and sse support isn't requested to avoid compile errors on ARM systems such as Apple Silicon

--- vector/src/simd_extensions/mod.rs
+++ vector/src/simd_extensions/mod.rs
@@ -342,9 +342,9 @@ macro_rules! sel_reg(
 		// TODO enable AVX512 detection again as soon as stdsimd is stable
         /*if is_x86_feature_detected!("avx512vl") && cfg!(feature="use_avx512") {
             $self_.$method(RegType::<<$type as ToSimd>::RegAvx512>::new(), $($args),*)
-        } else*/ if is_x86_feature_detected!("avx2") && cfg!(feature="use_avx2") {
+        } else*/ if cfg!(feature="use_avx2") && is_x86_feature_detected!("avx2") {
             $self_.$method(RegType::<<$type as ToSimd>::RegAvx>::new(), $($args),*)
-        } else if is_x86_feature_detected!("sse2") && cfg!(feature="use_sse2") {
+        } else if cfg!(feature="use_sse2") && is_x86_feature_detected!("sse2") {
             $self_.$method(RegType::<<$type as ToSimd>::RegSse>::new(), $($args),*)
         } else {
             $self_.$method(RegType::<<$type as ToSimd>::RegFallback>::new(), $($args),*)
@@ -353,9 +353,9 @@ macro_rules! sel_reg(
     ($method: ident::<$type: ident>($($args: expr),*)) => {
         /*if is_x86_feature_detected!("avx512vl") && cfg!(feature="use_avx512") {
             $method(RegType::<<$type as ToSimd>::RegAvx512>::new(), $($args),*)
-        } else*/ if is_x86_feature_detected!("avx2") && cfg!(feature="use_avx2") && cfg!(target_feature="avx2") {
+        } else*/ if cfg!(feature="use_avx2") && cfg!(target_feature="avx2") && is_x86_feature_detected!("avx2") {
             $method(RegType::<<$type as ToSimd>::RegAvx>::new(), $($args),*)
-        } else if is_x86_feature_detected!("sse2") && cfg!(feature="use_sse2")&& cfg!(target_feature="sse2") {
+        } else if cfg!(feature="use_sse2")&& cfg!(target_feature="sse2") && is_x86_feature_detected!("sse2") {
             $method(RegType::<<$type as ToSimd>::RegSse>::new(), $($args),*)
         } else {
             $method(RegType::<<$type as ToSimd>::RegFallback>::new(), $($args),*)
